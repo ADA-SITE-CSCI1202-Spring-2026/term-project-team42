b6f3a628bf431bbf11ee57c4ce0455f89e92f364
shajiyev <sabir.haciyev613@gmail.com>
2026-04-26 23:07:53 +0400

    Updated Baggage Handler and Fueling Truck

--- logic/BaggageHandler.java
+++ logic/BaggageHandler.java
@@ -1,12 +1,22 @@
 package logic;
 
-import airplanes.Aircraft;
+import airplanes.*;
 
 public class BaggageHandler implements IGroundService {
 
     @Override
-    public void serviceFlight(Aircraft aircraft) {
+    public void serviceFlight(FlightRequest request) {
+        if (!canProcess(request)) return;
+
+        Aircraft aircraft = request.getAircraft();
 
         System.out.println("BaggageHandler: Handling luggage for flight " + aircraft.getFlightNumber());
+    }
+
+    @Override
+    public boolean canProcess(FlightRequest request) {
+        Aircraft aircraft = request.getAircraft();
+
+        return (aircraft instanceof CommercialJet || aircraft instanceof PrivateCharter);
     }
 }
--- logic/FuelingTruck.java
+++ logic/FuelingTruck.java
@@ -1,13 +1,20 @@
 package logic;
 
-import airplanes.Aircraft;
+import airplanes.*;
 
 public class FuelingTruck implements IGroundService {
 
     @Override
-    public void serviceFlight(Aircraft aircraft) {
-        double fuelNeeded = aircraft.getRequiredFuel();
+    public void serviceFlight(FlightRequest request) {
+        if (!canProcess(request)) return;
 
-        System.out.println("FuelingTruck: Refueling flight " + aircraft.getFlightNumber() + " with " + fuelNeeded + "L of fuel.");
+        Aircraft aircraft = request.getAircraft();
+
+        System.out.println("FuelingTruck: Refueling flight " + aircraft.getFlightNumber() + " with " + aircraft.getRequiredFuel() + "L of fuel.");
+    }
+
+    @Override
+    public boolean canProcess(FlightRequest request) {
+        return true;
     }
 }
